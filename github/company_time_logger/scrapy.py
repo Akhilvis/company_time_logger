2f9b6d63f38cae35fe86f093781eb9d7df398e32
akhilvis <akhil@goodbits.in>
2020-02-07 12:54:10 +0530

    multiple users handled

--- github/company_time_logger/scrapy.py
+++ github/company_time_logger/scrapy.py
@@ -1,10 +1,10 @@
 from datetime import date, datetime
 import smtplib
 import datetime as main_datetime
+
 today = date.today()
 today_date = int(today.strftime("%d"))
-# today_date = 5
-punch_list = []
+# today_date = 6
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,7 +13,8 @@ chrome_options = Options()
 # chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/akhilvis/Documents/smartoffice/company_time_logger/chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('/home/akhilvis/Documents/smartoffice/company_time_logger/chromedriver',
+                          chrome_options=chrome_options)
 # driver = webdriver.Chrome('/home/akhil/Documents/company_time_logger/chromedriver', chrome_options=chrome_options)
 
 url = "http://www.so365.in/Goodbits_ESS"
@@ -24,6 +25,7 @@ user_accounts = [('akhil', 'akhil123'), ('krishnaprasad', 'kp123')]
 class IntimeCalc:
 
     def smartoffice_login(self, username, password):
+        punch_list = []
         driver.get(url)
         driver.find_element_by_id("txtUserName").send_keys(username)
         driver.find_element_by_id("txtPassword").send_keys(password)
@@ -63,14 +65,15 @@ class IntimeCalc:
         date_string = table.find_element_by_css_selector(last_element).text
         date_string_modified = date_string.split()
         self.last_date_obj = datetime.strptime(date_string_modified[0] + ' ' + date_string_modified[1],
-                                     '%d-%b-%Y %H:%M:%S')
-        punch_list.insert(0, self.last_date_obj)
+                                               '%d-%b-%Y %H:%M:%S')
+        if self.last_date_obj.day == today_date:
+            punch_list.insert(0, self.last_date_obj)
         return punch_list
 
     def calculate_intime(self, todays_punch_list):
         total_intime, total_outitme = 0, 0
         date_objects = list(reversed(todays_punch_list))
-        print(888888888888, date_objects)
+        # print('todays_punch_list>>>>>>>>>>>>>>>>>..', date_objects)
         len_dates = len(date_objects)
         for index, x in enumerate(date_objects):
             if index + 1 < len_dates:
@@ -93,9 +96,11 @@ class IntimeCalc:
         min, sec = divmod(seconds, 60)
         hour, min = divmod(min, 60)
 
-        last_out_time_calculated = main_datetime.timedelta(hours=self.last_date_obj.hour, minutes=self.last_date_obj.minute,
-                               seconds=self.last_date_obj.second) + main_datetime.timedelta(hours=hour, minutes=min,
-                                                                                       seconds=0)
+        last_out_time_calculated = main_datetime.timedelta(hours=self.last_date_obj.hour,
+                                                           minutes=self.last_date_obj.minute,
+                                                           seconds=self.last_date_obj.second) + main_datetime.timedelta(
+            hours=hour, minutes=min,
+            seconds=0)
         return last_out_time_calculated
 
     def send_mail(self):
@@ -113,21 +118,22 @@ class IntimeCalc:
         print("Email has been sent to ", rec_email)
 
 
-time_cal_obj = IntimeCalc()
 
 for user in user_accounts:
+    time_cal_obj = IntimeCalc()
+    todays_punch_list = []
     todays_punch_list = time_cal_obj.smartoffice_login(user[0], user[1])
-    print('todays_punch_list>>>>>>>>>>>>>>>>>..', todays_punch_list)
     total_intime, total_outitme = time_cal_obj.calculate_intime(todays_punch_list)
 
-    net_in_time_seconds = total_intime - (total_outitme - 3600)
+    net_in_time_seconds = total_intime - (total_outitme - 3600) if total_outitme > 3600 else total_intime
     net_in_time = time_cal_obj.convert_hours(net_in_time_seconds)
     extra_in_time_required = time_cal_obj.convert_hours(28800 - net_in_time_seconds)
     last_out_punch_time = time_cal_obj.add_time(28800 - net_in_time_seconds)
-
-
+    print('========================---  {0}  ---========================================'.format(user[0]))
     print('====total_intime======', time_cal_obj.convert_hours(total_intime))
     print('=====total_outitme=====', time_cal_obj.convert_hours(total_outitme))
     print('net_in_time>>>>>>>>>>>>>>>>>>>>>>>  ', net_in_time)
     print('extra_in_time_required>>>>>>>>>>>>>>>>>>>>>>>  ', extra_in_time_required)
     print('last_out_punch_time>>>>>>>>>>>>>>>>>>>>>>>  ', last_out_punch_time)
+    print('=============================================================================')
+# time_cal_obj.send_mail()
